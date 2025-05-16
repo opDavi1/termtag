cbfe672c5be1946b9f667a69c651ba0d68fe9990
opDavi1 <davissherman2007@gmail.com>
2025-05-16 11:34:02 -0700

    chore: add copyright notice to source files

--- src/formats/flac/mod.rs
+++ src/formats/flac/mod.rs
@@ -1,3 +1,6 @@
+// This file is part of termtag by Davis Sherman <davissherman2007@gmail.com> licensed under the GPL-3.0-or-later license.
+// See the included LICENSE file or go to <https://www.gnu.org/licenses/> for more information.
+
 use std::{
     fs,
     io::{Error, ErrorKind},
